2531ca032a009597bfb9baec2d8ecbcb994908b0
Alex Muro <am3081@gmail.com>
2023-02-07 13:48:55 -0500

    test route

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.get('/test', (req, res) => {
+    console.log("Just got a request!")
+    res.send({"hello": "world"})
+})
+
+
 app.listen(process.env.PORT || 3000)
